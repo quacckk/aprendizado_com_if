d70a48e44a7623851eb8e75acd59b3333111f1e0
Victor Lima <joaozinhobaodebriga@gmail.com>
2025-10-22 17:31:04 -0300

    Add files via upload
    código finalizado

---
+++ exercício1.py
@@ -0,0 +1,7 @@
+num = float(input("Digite um número qualquer: "))
+if num >= 100 and num <=200:
+    print("Seu número está no intervalo entre 100 e 200")
+    print(num)
+else:
+    print("Seu número não está no intervalo entre 100 e 200")
+    print(num)
